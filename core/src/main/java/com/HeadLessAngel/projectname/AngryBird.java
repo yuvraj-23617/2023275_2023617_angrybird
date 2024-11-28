c0ae7209e9dc198c2e06c375ba8e7ebbe8d0b140
kartik haritwal <159466017+Kartikharitwal@users.noreply.github.com>
2024-11-28 21:10:36 +0530

    Update AngryBird.java
    won screen added

--- core/src/main/java/com/HeadLessAngel/projectname/AngryBird.java
+++ core/src/main/java/com/HeadLessAngel/projectname/AngryBird.java
@@ -1,3 +1,4 @@
+// AngryBird (Game) Class
 package com.HeadLessAngel.projectname;
 
 import com.badlogic.gdx.Game;
@@ -7,12 +8,17 @@ public class AngryBird extends Game {
     public SpriteBatch batch;
     private StartScreen startScreen;
     private MainMenuScreen mainMenuScreen;
+    private WinScreen winScreen;
+    private LoseScreen loseScreen;
+    private int currentLevel = 1;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         startScreen = new StartScreen(this);
         mainMenuScreen = new MainMenuScreen(this);
+        winScreen = new WinScreen(this);
+        loseScreen = new LoseScreen(this);
         setScreen(startScreen);
     }
 
@@ -20,10 +26,28 @@ public class AngryBird extends Game {
         setScreen(mainMenuScreen);
     }
 
+    public int getCurrentLevel() {
+        return currentLevel;
+    }
+
+    public void setCurrentLevel(int level) {
+        this.currentLevel = level;
+    }
+
+    public void switchToWinScreen() {
+        setScreen(winScreen);
+    }
+
+    public void switchToLoseScreen() {
+        setScreen(loseScreen);
+    }
+
     @Override
     public void dispose() {
         batch.dispose();
         startScreen.dispose();
         mainMenuScreen.dispose();
+        winScreen.dispose();
+        loseScreen.dispose();
     }
 }
