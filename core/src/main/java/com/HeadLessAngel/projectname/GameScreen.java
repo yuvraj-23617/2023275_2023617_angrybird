62094b3d68d907baca920c4e25de39ec4f3de1a8
yuvraj-23617 <yuvraj23617@iiitd.ac.in>
2024-11-29 16:22:50 +0530

    catapult added, and level managed more steadily

--- core/src/main/java/com/HeadLessAngel/projectname/GameScreen.java
+++ core/src/main/java/com/HeadLessAngel/projectname/GameScreen.java
@@ -1,4 +1,3 @@
-// GameScreen Class
 package com.HeadLessAngel.projectname;
 
 import com.badlogic.gdx.Gdx;
@@ -13,6 +12,7 @@ public class GameScreen implements Screen {
     private final AngryBird game;
     private final int level;
 
+    private Texture catapultTexture;
     private Texture background;
     private Texture groundTexture;
     private Texture birdTexture;
@@ -20,24 +20,29 @@ public class GameScreen implements Screen {
 
     private Rectangle groundBounds;
     private Rectangle birdBounds;
-
-
     private Rectangle pigBounds;
     private Rectangle pigBounds2;
-    private boolean pigAlive2;
 
     private Vector2 birdPosition;
     private Vector2 birdVelocity;
     private Vector2 initialPosition;
 
     private final Vector2 gravity = new Vector2(0, -400); // Gravity force
+
     private boolean birdLaunched;
     private boolean pigAlive;
+    private boolean pigAlive2;
     private boolean birdAlive;
     private boolean isDragging;
+    private boolean levelFinished;
 
     private int birdsLeft;
-    private boolean levelFinished;
+
+    // New variables for catapult positioning
+    private int catapultX;
+    private int catapultY;
+    private int catapultWidth;
+    private int catapultHeight;
 
     public GameScreen(AngryBird game, int level) {
         this.game = game;
@@ -46,15 +51,13 @@ public class GameScreen implements Screen {
         // Initialize textures and positions based on the level
         loadLevelAssets(level);
 
-        initialPosition = new Vector2(150, 300); // Initial position near the slingshot
         birdPosition = new Vector2(initialPosition);
         birdVelocity = new Vector2(0, 0);
         birdBounds = new Rectangle(birdPosition.x, birdPosition.y, birdTexture.getWidth(), birdTexture.getHeight());
 
         groundBounds = new Rectangle(0, 0, Gdx.graphics.getWidth(), groundTexture.getHeight());
-        pigBounds = new Rectangle(600, 200, pigTexture.getWidth(), pigTexture.getHeight());
-        pigBounds2 = new Rectangle(800, 50, pigTexture.getWidth(), pigTexture.getHeight()); // Adjust position as needed
-        pigAlive2 = true;
+
+        pigAlive2 = level == 2 || level == 3; // Pigs in level 2 and 3 have two pigs
         pigAlive = true;
         birdAlive = true;
         birdLaunched = false;
@@ -68,21 +71,55 @@ public class GameScreen implements Screen {
         // Load level-specific assets
         switch (level) {
             case 1:
-                background = new Texture(Gdx.files.internal("level_1.png"));
+                background = new Texture(Gdx.files.internal("level_1(1).png"));
                 pigTexture = new Texture(Gdx.files.internal("pig.png"));
-                pigAlive2=false;
+                catapultTexture = new Texture(Gdx.files.internal("catapult.png"));
+                initialPosition = new Vector2(150, 300);
+
+                // Catapult position for level 1
+                catapultX = 100;  // X position
+                catapultY = 200;  // Y position
+                catapultWidth = 100;
+                catapultHeight = 200;
+
+                pigBounds = new Rectangle(600, 200, pigTexture.getWidth(), pigTexture.getHeight());
+                pigAlive2 = false;
                 break;
+
             case 2:
-                background = new Texture(Gdx.files.internal("level_2.png"));
+                background = new Texture(Gdx.files.internal("level_2(1).png"));
                 pigTexture = new Texture(Gdx.files.internal("pig.png"));
-                pigTexture = new Texture(Gdx.files.internal("pig.png"));
-                pigAlive2=true;
+                catapultTexture = new Texture(Gdx.files.internal("catapult.png"));
+                initialPosition = new Vector2(200, 350);
+
+                // Catapult position for level 2
+                catapultX = 250;  // Different X position
+                catapultY = 250;  // Different Y position
+                catapultWidth = 120;
+                catapultHeight = 180;
+
+                pigBounds = new Rectangle(700, 250, pigTexture.getWidth(), pigTexture.getHeight());
+                pigBounds2 = new Rectangle(900, 100, pigTexture.getWidth(), pigTexture.getHeight());
+                pigAlive2 = true;
                 break;
+
             case 3:
-                background = new Texture(Gdx.files.internal("level_3.png"));
+                background = new Texture(Gdx.files.internal("level_3(1).png"));
                 pigTexture = new Texture(Gdx.files.internal("pig.png"));
-                pigAlive2=true;
+                catapultTexture = new Texture(Gdx.files.internal("catapult.png"));
+                initialPosition = new Vector2(100, 400);
+
+                // Catapult position for level 3
+                catapultX = 50;   // Different X position
+                catapultY = 300;  // Different Y position
+                catapultWidth = 90;
+                catapultHeight = 220;
+
+                pigBounds = new Rectangle(500, 150, pigTexture.getWidth(), pigTexture.getHeight());
+                pigBounds2 = new Rectangle(800, 300, pigTexture.getWidth(), pigTexture.getHeight());
+                pigAlive2 = true;
                 break;
+
             default:
                 throw new IllegalArgumentException("Invalid level: " + level);
         }
@@ -128,14 +165,22 @@ public class GameScreen implements Screen {
         game.batch.begin();
         game.batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-        // Render the ground
+        // Draw catapult for all levels
+        game.batch.draw(
+            catapultTexture,
+            catapultX,
+            catapultY,
+            catapultWidth,
+            catapultHeight
+        );
+
         game.batch.draw(groundTexture, 0, 0, Gdx.graphics.getWidth(), groundTexture.getHeight());
 
         // Render the pig only if it's alive
         if (pigAlive) {
             game.batch.draw(pigTexture, pigBounds.x, pigBounds.y);
         }
-        if (pigAlive2 && level == 2) { // Render pig2 only if it's alive and we are in level 2
+        if (pigAlive2 && (level == 2 || level == 3)) { // Render pig2 in levels 2 and 3
             game.batch.draw(pigTexture, pigBounds2.x, pigBounds2.y);
         }
 
@@ -198,14 +243,13 @@ public class GameScreen implements Screen {
         }
     }
 
-
-
     @Override
     public void dispose() {
         background.dispose();
         groundTexture.dispose();
         birdTexture.dispose();
         pigTexture.dispose();
+        catapultTexture.dispose(); // Don't forget to dispose of the catapult texture
     }
 
     @Override public void show() {}
